7792b5c8fdd1c71d19ae91f27046aab798e357bc
MAZI_ <dnl.mazur@gmail.com>
2022-07-12 22:42:46 +0200

    Init project

---
+++ src/main/java/org/home/mazi/sampledockerbuildspringboot/SampleDockerBuildSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.home.mazi.sampledockerbuildspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleDockerBuildSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleDockerBuildSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/home/mazi/sampledockerbuildspringboot/SampleDockerBuildSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.home.mazi.sampledockerbuildspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleDockerBuildSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
